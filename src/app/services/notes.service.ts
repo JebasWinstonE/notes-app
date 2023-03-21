1b97cebbad7cbae23ad4b731bddc0b5d42aa63ca
Jebas Winston <jebaswinston55@gmail.com>
2023-03-22 01:41:49 +0530

    Minor tweek in creating notes

--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -1,11 +1,14 @@
-import { EventEmitter } from "@angular/core";
-import { Note } from "../models/note.model";
+import { EventEmitter } from '@angular/core';
+import { Note } from '../models/note.model';
 
 export class NotesService {
-
   notes: Note[] = [];
   insertNote(title: string, desc: string) {
-    if (title !== '' && desc !== '') {
+    if (
+      (title.length > 0 && desc.length > 0) ||
+      (title.length > 0 && desc.length === 0) ||
+      (title.length === 0 && desc.length > 0)
+    ) {
       this.notes.push(new Note(title, desc));
     }
   }
